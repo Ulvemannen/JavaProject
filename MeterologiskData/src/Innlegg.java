5b15773047c3116fa25b0f748852d04c31509c4b
Jonas Bergem <Jonasbergem@gmail.com>
2011-04-04 10:03:49 +0200

    Added JavaProject (Eclipse) and Innlegg.java

---
+++ MeterologiskData/src/Innlegg.java
@@ -0,0 +1,26 @@
+//Hendelseklassen hvor generell informasjon om hendelsen skal lagres.
+public class Innlegg {
+	//Deklarere variabler.
+	int nedbør;
+	double mintemp;
+	double maxtemp;
+	
+	//Konstruktør for Innlegg-objektet.
+	public Innlegg(int n,double min,double max){
+		nedbør = n;
+		mintemp = min;
+		maxtemp = max;
+	}
+	//Returnerer nedbørsmengde for objektet.
+	public int getNedbør(){
+		return nedbør;
+	}
+	//Returnerer minimumstemperaturen for objektet.
+	public double getMin(){
+		return mintemp;
+	}
+	//Returnerer makstemperatur for objektet.
+	public double getMax(){
+		return maxtemp;
+	}
+}
