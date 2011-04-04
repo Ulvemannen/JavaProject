1fbd0af48ae984d1aef927d4ed125092294b369e
Jonas Bergem <Jonasbergem@gmail.com>
2011-04-04 10:16:30 +0200

    La til det vi hadde gjort before git

---
+++ MeterologiskData/src/MeterologiskMain.java
@@ -0,0 +1,13 @@
+//Klassen til mainmetoden.
+public class MeterologiskMain {
+	/**
+	 * @Fredrik T Lillejodet s169983 og Jonas Bergem s169950.
+	 * Prosjekt i Programutvikling LO137A / LO137I HIO Våren 2011.
+	 * Faglærer Eva H Vihovde.
+	 */
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		//Oppretter ett GUI objekt hvor alt av grensesnitt ligger.
+		MetrologiskGui mm = new MetrologiskGui();
+	}
+}
